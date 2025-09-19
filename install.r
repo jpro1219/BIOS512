c88444f8ad7c5f9d177d63e49e825c1947095181
Jonah Prober <156374676+jpro1219@users.noreply.github.com>
2025-09-18 23:44:42 -0400

    Update install.r

--- install.r
+++ install.r
@@ -1,5 +1,6 @@
 install.packages("tidyverse", dependencies = TRUE)
 install.packages("rmarkdown", dependencies = TRUE)
 install.packages("IRkernel", dependencies = TRUE)
+install.packages("skimr", dependencies = TRUE)
 
 IRkernel::installspec(user = FALSE)
